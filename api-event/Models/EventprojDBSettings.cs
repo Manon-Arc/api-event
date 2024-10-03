d8f7d29940c87ec3530d7552fa6c09fddd243910
Manon-Arc <112950709+Manon-Arc@users.noreply.github.com>
2024-10-03 13:48:09 +0200

    add configuration db model

---
+++ api-event/Models/EventprojDBSettings.cs
@@ -0,0 +1,10 @@
+﻿namespace api_event;
+
+public class EventprojDBSettings
+{
+    public string ConnectionString { get; set; } = null!;
+
+    public string DatabaseName { get; set; } = null!;
+
+    public string BooksCollectionName { get; set; } = null!;
+}
--- api-event/Program.cs
+++ api-event/Program.cs
@@ -1,37 +1,7 @@
-using Microsoft.AspNetCore.Authentication.JwtBearer;
-using Microsoft.IdentityModel.Tokens;
-using System.Text;
+using api_event;
 
-public class Startup
-{
-    public void ConfigureServices(IServiceCollection services)
-    {
-        services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
-            .AddJwtBearer(options =>
-            {
-                options.TokenValidationParameters = new TokenValidationParameters
-                {
-                    ValidateIssuer = true,
-                    ValidateAudience = true,
-                    ValidateLifetime = true,
-                    ValidateIssuerSigningKey = true,
-                    ValidIssuer = Configuration["Jwt:Issuer"],
-                    ValidAudience = Configuration["Jwt:Audience"],
-                    IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(Configuration["Jwt:Key"]))
-                };
-            });
+var builder = WebApplication.CreateBuilder(args);
 
-        services.AddControllers();
-    }
-
-    public void Configure(IApplicationBuilder app, IHostingEnvironment env)
-    {
-        app.UseAuthentication(); // Enable JWT authentication
-        app.UseAuthorization();
-
-        app.UseEndpoints(endpoints =>
-        {
-            endpoints.MapControllers();
-        });
-    }
-}
+// Add services to the container.
+builder.Services.Configure<EventprojDBSettings>(
+    builder.Configuration.GetSection("EventprojDB"));
