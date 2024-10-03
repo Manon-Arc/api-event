6ed4a92743b61377603b505a29e15be56a64ab5a
LeoM <54435611+Tenlutshy@users.noreply.github.com>
2024-10-03 14:30:35 +0200

    Create user controller

---
+++ api-event/Controllers/User.controller.cs
@@ -0,0 +1,30 @@
+using api_event.Models;
+using BookStoreApi.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace api_event.Controllers;
+
+[Route("/[UserController.routeName]")]
+[ApiController]
+public class UserController : ControllerBase
+{
+    private readonly UsersService _usersService;
+    static String routeName = "users";
+
+    public UserController( UsersService usersService)
+    {
+        _usersService = usersService;
+    }
+
+    [HttpGet]
+    public async Task<ActionResult<IEnumerable<User>>> GetUsers()
+    {
+        return await _usersService.GetAsync();
+    }
+
+    [HttpGet("{id}")]
+    public async Task<ActionResult<User>> GetUser(String id)
+    {
+        return (await _usersService.GetAsync(id))!;
+    }
+}
