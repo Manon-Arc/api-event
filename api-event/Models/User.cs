dfc4a6ccba83844bf4059bf26924b3acf8e2ea9f
Manon-Arc <112950709+Manon-Arc@users.noreply.github.com>
2024-10-03 13:29:21 +0200

    add models

---
+++ api-event/Models/User.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.VisualBasic.CompilerServices;
+using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+
+namespace api_event.Models;
+
+public class User
+{
+    [BsonId]
+    [BsonRepresentation(BsonType.ObjectId)]
+    public string? Id { get; set; }
+
+    public string FirstName { get; set; } = null!;
+    
+    public string LastName { get; set; } = null!;
+    
+    public string Mail { get; set; } = null!;
+    
+    public int? Permission { get; set; } = null!;
+    
+}
