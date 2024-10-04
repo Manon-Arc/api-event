88688d9c3b11065bb9e2cf0bc7b49d849252d881
Mathieu Barboteau <mathieu.barboteau@ynov.com>
2024-10-04 16:34:27 +0200

    EventDtoClass

--- api-event/Models/CredentialsPOSTDto.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace api_event.Models
-{
-    public class CredentialsPOSTDto
-    {
-        public string Mail { get; set; } = string.Empty;
-        public string Password { get; set; } = string.Empty;
-
-    }
-}
