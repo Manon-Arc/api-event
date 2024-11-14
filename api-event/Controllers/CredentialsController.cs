633d3024c2ba65529d9d9bbd0137c92456e7fe2b
LeoM <54435611+Tenlutshy@users.noreply.github.com>
2024-11-14 10:00:42 +0100

    [CHORE] Passe data in query instead of body

--- api-event/Controllers/CredentialsController.cs
+++ api-event/Controllers/CredentialsController.cs
@@ -2,38 +2,31 @@
 using api_event.Services;
 using Microsoft.AspNetCore.Mvc;
 
-namespace api_event.Controllers
+namespace api_event.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class CredentialController : ControllerBase
 {
-    [ApiController]
-    [Route("[controller]")]
-    public class CredentialController : ControllerBase
+    private readonly CredentialsService _credentialService;
+
+    public CredentialController(CredentialsService credentialService)
     {
-        private readonly CredentialsService _credentialService;
+        _credentialService = credentialService;
+    }
 
-        public CredentialController(CredentialsService credentialService)
-        {
-            _credentialService = credentialService;
-        }
+    [HttpPost("register")]
+    public async Task<IActionResult> Register([FromQuery] CredentialsModel credential)
+    {
+        await _credentialService.RegisterAsync(credential);
+        return Ok("Utilisateur enregistré avec succès");
+    }
 
-        // Endpoint pour enregistrer un nouvel utilisateur
-        [HttpPost("register")]
-        
-        public async Task<IActionResult> Register([FromQuery] CredentialsModel _credential)
-        {
-            await _credentialService.RegisterAsync(_credential);
-            return Ok("Utilisateur enregistré avec succès");
-        }
-
-        // Endpoint pour se connecter et obtenir un token JWT
-        [HttpPost("login")]
-        public async Task<IActionResult> Login([FromBody] CredentialsModel credentials)
-        {
-            var token = await _credentialService.LoginAsync(credentials);
-            if (token == null)
-            {
-                return Unauthorized("Identifiants invalides");
-            }
-            return Ok(new { Token = token });
-        }
+    [HttpPost("login")]
+    public async Task<IActionResult> Login([FromQuery] CredentialsModel credentials)
+    {
+        var token = await _credentialService.LoginAsync(credentials);
+        if (token == null) return Unauthorized("Identifiants invalides");
+        return Ok(new { Token = token });
     }
 }
